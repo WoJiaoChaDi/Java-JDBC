3e419663bf21be01487edb3c7af130b03ce93298
wojiaochadi <264835229@qq.com>
2020-01-19 11:07:17 +0800

    封装DriverManager获取数据库连接方法

--- JDBC-01/src/main/java/com/atguigu/jdbc/Jdbc_Test.java
+++ JDBC-01/src/main/java/com/atguigu/jdbc/Jdbc_Test.java
@@ -115,4 +115,34 @@ public class Jdbc_Test {
         System.out.println(connection);
         //System.out.println(connection2);
     }
+
+    /**
+     * 封装DriverManager获取数据库连接
+     * @return
+     * @throws Exception
+     */
+    public Connection getConnection2() throws Exception{
+        //1. 准备连接数据库的 4 个字符串.
+        //1). 创建 Properties 对象
+        Properties properties = new Properties();
+
+        //2). 获取 jdbc.properties 对应的输入流
+        InputStream in =
+                this.getClass().getClassLoader().getResourceAsStream("jdbc.properties");
+
+        //3). 加载 2） 对应的输入流
+        properties.load(in);
+
+        //4). 具体决定 user, password 等4 个字符串.
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        String jdbcUrl = properties.getProperty("jdbcUrl");
+        String driver = properties.getProperty("driver");
+
+        //2. 加载数据库驱动程序(对应的 Driver 实现类中有注册驱动的静态代码块.)
+        Class.forName(driver);
+
+        //3. 通过 DriverManager 的 getConnection() 方法获取数据库连接.
+        return DriverManager.getConnection(jdbcUrl, user, password);
+    }
 }
