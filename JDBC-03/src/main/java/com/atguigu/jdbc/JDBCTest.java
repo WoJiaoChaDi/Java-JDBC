0f0a51fefe8a02579d528c2869b5479c181c1cb7
wojiaochadi <264835229@qq.com>
2020-01-20 17:27:12 +0800

    DBCP数据库连接池

--- JDBC-03/src/main/java/com/atguigu/jdbc/JDBCTest.java
+++ JDBC-03/src/main/java/com/atguigu/jdbc/JDBCTest.java
@@ -1,11 +1,17 @@
 package com.atguigu.jdbc;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.BasicDataSourceFactory;
 import org.junit.Test;
 
+import javax.sql.DataSource;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class JDBCTest {
 
@@ -140,4 +146,98 @@ public class JDBCTest {
 		}
 	}
 
+    /**
+     * 使用 DBCP 数据库连接池
+     * 1. 加入 jar 包(2 个jar 包). 依赖于 Commons Pool
+     * 2. 创建数据库连接池
+     * 3. 为数据源实例指定必须的属性
+     * 4. 从数据源中获取数据库连接
+     * @throws SQLException
+     */
+    @Test
+    public void testDBCP() throws SQLException{
+        final BasicDataSource dataSource = new BasicDataSource();
+
+        //2. 为数据源实例指定必须的属性
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        dataSource.setUrl("jdbc:mysql://192.168.198.144/atguigu");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+        //3. 指定数据源的一些可选的属性.
+        //1). 指定数据库连接池中初始化连接数的个数
+        dataSource.setInitialSize(5);
+
+        //2). 指定最大的连接数: 同一时刻可以同时向数据库申请的连接数
+        dataSource.setMaxActive(5);
+
+        //3). 指定小连接数: 在数据库连接池中保存的最少的空闲连接的数量
+        dataSource.setMinIdle(2);
+
+        //4).等待数据库连接池分配连接的最长时间. 单位为毫秒. 超出该时间将抛出异常.
+        dataSource.setMaxWait(1000 * 5);
+
+        //4. 从数据源中获取数据库连接
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection.getClass());
+
+        connection = dataSource.getConnection();
+        System.out.println(connection.getClass());
+
+        connection = dataSource.getConnection();
+        System.out.println(connection.getClass());
+
+        connection = dataSource.getConnection();
+        System.out.println(connection.getClass());
+
+        Connection connection2 = dataSource.getConnection();
+        System.out.println(">" + connection2.getClass());
+
+        new Thread(){
+            public void run() {
+                Connection conn;
+                try {
+                    conn = dataSource.getConnection();
+                    System.out.println(conn.getClass());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            };
+        }.start();
+
+        try {
+            Thread.sleep(5500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        connection2.close();
+    }
+
+	/**
+	 * 1. 加载 dbcp 的 properties 配置文件: 配置文件中的键需要来自 BasicDataSource
+	 * 的属性.
+	 * 2. 调用 BasicDataSourceFactory 的 createDataSource 方法创建 DataSource
+	 * 实例
+	 * 3. 从 DataSource 实例中获取数据库连接.
+	 */
+	@Test
+	public void testDBCPWithDataSourceFactory() throws Exception{
+
+		Properties properties = new Properties();
+		InputStream inStream = JDBCTest.class.getClassLoader()
+				.getResourceAsStream("dbcp.properties");
+		properties.load(inStream);
+
+		DataSource dataSource =
+				BasicDataSourceFactory.createDataSource(properties);
+
+		System.out.println(dataSource.getConnection());
+
+		//测试
+//		BasicDataSource basicDataSource =
+//				(BasicDataSource) dataSource;
+//
+//		System.out.println(basicDataSource.getMaxWait());
+	}
 }
