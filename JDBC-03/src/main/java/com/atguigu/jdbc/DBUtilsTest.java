011bd379f4d58b16773091c8552f6d21b3e3d034
wojiaochadi <264835229@qq.com>
2020-01-21 10:16:24 +0800

    DBUtils  ResultSetHandler接口

--- JDBC-03/src/main/java/com/atguigu/jdbc/DBUtilsTest.java
+++ JDBC-03/src/main/java/com/atguigu/jdbc/DBUtilsTest.java
@@ -1,9 +1,15 @@
 package com.atguigu.jdbc;
 
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 测试 DBUtils 工具类
@@ -35,7 +41,57 @@ public class DBUtilsTest {
 		} finally{
 			JDBCTools.releaseDB(null, null, connection);
 		}
-		
 	}
+
+    /**
+     * 测试 QueryRunner 的 query 方法
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void testResultSetHandler(){
+        String sql = "SELECT id, name, email, birth " +
+                "FROM customers";
+
+        //1. 创建 QueryRunner 对象
+        QueryRunner queryRunner = new QueryRunner();
+
+        Connection conn = null;
+
+        try {
+            conn = JDBCTools.getConnection();
+            /**
+             * 2. 调用 query 方法:
+             * ResultSetHandler 参数的作用: query 方法的返回值直接取决于
+             * ResultSetHandler 的 hanlde(ResultSet rs) 是如何实现的. 实际上, 在
+             * QueryRunner 类的 query 方法中也是调用了 ResultSetHandler 的 handle()
+             * 方法作为返回值的。
+             */
+            Object object = queryRunner.query(conn, sql,
+                new ResultSetHandler(){
+                    @Override
+                    public Object handle(ResultSet rs) throws SQLException {
+                        List<Customer> customers = new ArrayList<>();
+
+                        while(rs.next()){
+                            int id = rs.getInt(1);
+                            String name = rs.getString(2);
+                            String email = rs.getString(3);
+                            Date birth = rs.getDate(4);
+
+                            Customer customer =
+                                    new Customer(id, name, email, birth);
+                            customers.add(customer);
+                        }
+                        return customers;
+                    }
+                }
+            );
+            System.out.println(object);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally{
+            JDBCTools.releaseDB(null, null, conn);
+        }
+    }
 
 }
